f330f0ad1991952aef6165238e92b64bd876e7ac
АсташинИА <@211-06>
2023-05-20 10:24:50 +0300

    Добавьте файлы проекта.

---
+++ Monte-Karlo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Monte_Karlo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, k;
+            double s, x, y;
+            k = 0;
+            n = 1000;
+            Random r = new Random();
+            for (int i = 1; i <= n; i++)
+            {
+                x = r.NextDouble() * 4 - 2;
+                y = r.NextDouble() * 4 - 2;
+                if (x * x + y * y <= 4)
+                {
+                    k = k + 1;
+                }
+            }
+            s = 4 * (k / n);
+            Console.WriteLine("Результат Пи = " + s);
+            Console.WriteLine("Точное Пи=" + Math.PI);
+        }
+    }
+}
